9c275c991ec655c7acf18b0c20c55c7b5fd274bf
Thomas Herrmann <herrmann@ftm.mw.tum.de>
2021-08-05 21:49:49 +0200

    fix pip install acados_template

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,12 +67,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-subprocess.call('git clone https://github.com/acados/acados.git', shell=True)
-subprocess.call('cd acados', shell=True)
-subprocess.call('git submodule update --recursive --init', shell=True)
-subprocess.call('mkdir -p build', shell=True)
-subprocess.call('cd build', shell=True)
-subprocess.call('cmake ..', shell=True)
-subprocess.call('make install', shell=True)
-subprocess.call('pip install interfaces/acados_template', shell=True)
-subprocess.call('cd ..', shell=True)
+subprocess.call('git clone --recurse-submodules https://github.com/acados/acados.git', shell=True)
+subprocess.call('mkdir -p acados/build', shell=True)
+subprocess.call('cmake acados/ && make install', shell=True)
+subprocess.call('pip install acados/interfaces/acados_template', shell=True)
