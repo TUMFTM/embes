9a4c00e58a5f5ad53a672aaafb4cb35366682e3f
Thomas Herrmann <herrmann@ftm.mw.tum.de>
2021-08-05 09:33:59 +0200

    temp: rm subprocess calls

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -66,7 +66,3 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-subprocess.call('git clone https://github.com/acados/acados.git', shell=True)
-subprocess.call('cd acados', shell=True)
-subprocess.call('git submodule update --recursive --init', shell=True)
